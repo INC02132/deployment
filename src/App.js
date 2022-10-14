bb3e5b058c7efec6af845820e50aaab8869807e7
naiyar imam <naiyar.imam@incture.com>
2022-10-14 09:39:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState("");
+
+  const fetchData = () => {
+    fetch("http://localhost:8080/getData").then((res) => res.json()).then((data) => {
+      setData(data)
+      setLoading(false);
+      }
+    ).catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchData();
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello world App
+      </h1>
+      {
+        loading ? <p>Loading Data</p> : <p></p>
+      }
     </div>
   );
 }
